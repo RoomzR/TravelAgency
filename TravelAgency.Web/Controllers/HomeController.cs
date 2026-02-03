12b86d025a4357e9510a3c3bfd391a182a4317b5
RoomzR <roomzrly@mail.ru>
2026-02-03 23:24:45 +0300

    Добавление HomeController

--- TravelAgency.Web/Controllers/HomeController.cs
+++ TravelAgency.Web/Controllers/HomeController.cs
@@ -1,34 +1,74 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using TravelAgency.DAL;
 using TravelAgency.BLL.Entities;
-using TravelAgency.Web.Models;
 using TravelAgency.DAL.Data;
+using TravelAgency.Web.Models;
+using TravelAgency.Web.Models.ViewModels;
 
-namespace TravelAgency.Controllers
+namespace TravelAgency.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ApplicationDbContext context, ILogger<HomeController> logger)
+        public HomeController(
+            ApplicationDbContext context,
+            ILogger<HomeController> logger)
         {
             _context = context;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var featuredTours = _context.Tours
-                .Where(t => t.IsActive)
+            var featuredTours = await _context.Tours
+                .Where(t => t.IsActive && t.IsHotDeal)
                 .Include(t => t.Country)
                 .Include(t => t.City)
                 .Include(t => t.HotelCategory)
+                .Take(4)
+                .ToListAsync();
+
+            var popularTours = await _context.Tours
+                .Where(t => t.IsActive)
+                .OrderByDescending(t => t.ViewsCount)
+                .Include(t => t.Country)
+                .Include(t => t.City)
                 .Take(6)
-                .ToList();
-                
-            return View(featuredTours);
+                .ToListAsync();
+
+            var countries = await _context.Countries
+                .Take(6)
+                .ToListAsync();
+
+            var news = await _context.NewsArticles
+                .OrderByDescending(n => n.CreatedDate)
+                .Take(3)
+                .ToListAsync();
+
+            var viewModel = new HomeViewModel
+            {
+                FeaturedTours = featuredTours,
+                PopularTours = popularTours,
+                Countries = countries,
+                NewsArticles = news,
+                TourCount = await _context.Tours.CountAsync(t => t.IsActive),
+                ClientCount = await _context.Users.CountAsync(),
+                BookingCount = await _context.Bookings.CountAsync()
+            };
+
+            return View(viewModel);
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
         }
 
         public IActionResult Privacy()
---
+++ TravelAgency.Web/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using TravelAgency.BLL.Entities;
+using System.Collections.Generic;
+
+namespace TravelAgency.Web.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Tour> FeaturedTours { get; set; } = new();
+        public List<Tour> PopularTours { get; set; } = new();
+        public List<Country> Countries { get; set; } = new();
+        public List<NewsArticle> NewsArticles { get; set; } = new();
+        public int TourCount { get; set; }
+        public int ClientCount { get; set; }
+        public int BookingCount { get; set; }
+    }
+}
